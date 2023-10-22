bc86c4c545ffbd22996397e09a36374852053562
seed4407 <124889238+seed4407@users.noreply.github.com>
2023-10-22 18:30:06 -0300

    Inicio listo

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,6 +6,7 @@ import { Col, Container, Row } from 'react-bootstrap';
 import admision from '../assets/usm-admision-1.jpeg'
 import informacion from '../assets/imagen-info.png'
 import carrera from '../assets/imagen-carrera.png'
+import valparaiso from '../assets/imagen-valpo.jpg'
 import Dropdown from 'react-bootstrap/Dropdown';
 
 export const HomePage = () => {
@@ -27,13 +28,14 @@ export const HomePage = () => {
         </Row>
         <Row>
           <Col>
-            <Card className='card-home' style={{height: "100%"}}>
+            <Card className='card-home'>
               <Container className='container-card'>
                 <Row>
-                  <Col>
-                    <Card.Img style={{objectFit:"scale-down"}} src={admision} />
+                  <Col xs={6}>
+                    <Card.Img style={{objectFit:"scale-down", width:"46%",height:"100%", paddingLeft:"20px",paddingRight:"3px",paddingBottom:"4px"}} src={admision} />
+                    <Card.Img style={{objectFit:"scale-down",width:"50%",paddingBottom:"6px"}} src={valparaiso} />
                   </Col>
-                  <Col>
+                  <Col xs={6}>
                     <Card.Body>
                       <Card.Title><h1 className='title'>Postula aqui</h1></Card.Title>
                       <Card.Text>
@@ -46,60 +48,56 @@ export const HomePage = () => {
               </Container>
             </Card>
           </Col>
+        </Row>
+        <Row>
           <Col>
-            <Row>
-              <Col>
-                <Card className='card-home' style={{marginBottom: "10px"}}>
-                  <Container className='container-card'>
-                    <Row>
-                      <Col>
-                        <Card.Img style={{objectFit:"fill"}} src={informacion} thumbnail />
-                      </Col>
-                      <Col>
-                        <Card.Body>
-                          <Card.Title><h1 className='subtitle'>Información general</h1></Card.Title>
-                          <Card.Text>
-                            <p className='plaintext'>Si deseas buscar informacion general de la Universidad, se cuenta con un repositorio donde se encuentra toda la informacion reunida por secciones</p>
-                          </Card.Text>
-                          <Button className='button' href="/repositorio">Ver mas</Button>
-                        </Card.Body>
-                      </Col>
-                    </Row>
-                  </Container>
-                </Card>
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <Card className='card-home'>
-                  <Container className='container-card'>
-                    <Row>
-                      <Col>
-                        <Card.Img style={{objectFit:"fill"}} src={carrera} thumbnail />
-                      </Col>
-                      <Col>
-                        <Card.Body>
-                          <Card.Title><h1 className='subtitle'>Carreras</h1></Card.Title>
-                          <Card.Text>
-                          <p className='plaintext'>Si deseas buscar informacion sobre una carrera en especifico impartida por la Universidad, se tiene un apartado dedicado a cada una</p>
-                          </Card.Text>
-                          <Dropdown>
-                            <Dropdown.Toggle className="button button-secundarios" id="dropdown-basic">
-                              Ver mas
-                              </Dropdown.Toggle>
-                              <Dropdown.Menu>
-                                <Dropdown.Item href="/informatica">Ingenieria Civil Informatica</Dropdown.Item>
-                                <Dropdown.Item href="/matematicas">Ingenieria Civil Matematicas</Dropdown.Item>
-                                <Dropdown.Item href="/minas">Ingenieria Civil en Minas</Dropdown.Item>
-                              </Dropdown.Menu>
-                            </Dropdown>
-                        </Card.Body>
-                      </Col>
-                    </Row>
-                  </Container>
-                </Card>
-              </Col>
-            </Row>
+            <Card className='card-home' style={{marginTop: "10px"}}>
+              <Container className='container-card'>
+                  <Row>
+                    <Col>
+                      <Card.Img style={{objectFit:"scale-down", paddingTop:"30px",paddingLeft:"20px"}} src={informacion}/>
+                    </Col>
+                    <Col>
+                      <Card.Body>
+                        <Card.Title><h1 className='subtitle'>Información general</h1></Card.Title>
+                        <Card.Text>
+                          <p className='plaintext'>Si deseas buscar informacion general de la Universidad, se cuenta con un repositorio donde se encuentra toda la informacion reunida por secciones</p>
+                        </Card.Text>
+                        <Button className='button' href="/repositorio">Ver mas</Button>
+                      </Card.Body>
+                    </Col>
+                  </Row>
+              </Container>
+            </Card>
+          </Col>
+          <Col>
+            <Card className='card-home' style={{marginTop: "10px"}}>
+              <Container className='container-card'>
+                <Row>
+                  <Col>
+                    <Card.Img style={{objectFit:"fill", paddingTop:"25px",paddingLeft:"20px"}} src={carrera} />
+                  </Col>
+                  <Col>
+                    <Card.Body>
+                      <Card.Title><h1 className='subtitle'>Carreras</h1></Card.Title>
+                      <Card.Text>
+                        <p className='plaintext'>Si deseas buscar informacion sobre una carrera en especifico impartida por la Universidad, se tiene un apartado dedicado a cada una</p>
+                      </Card.Text>
+                        <Dropdown>
+                          <Dropdown.Toggle className="button button-secundarios" id="dropdown-basic">
+                            Ver mas
+                          </Dropdown.Toggle>
+                          <Dropdown.Menu>
+                              <Dropdown.Item href="/informatica">Ingenieria Civil Informatica</Dropdown.Item>
+                              <Dropdown.Item href="/matematicas">Ingenieria Civil Matematicas</Dropdown.Item>
+                               <Dropdown.Item href="/minas">Ingenieria Civil en Minas</Dropdown.Item>
+                          </Dropdown.Menu>
+                        </Dropdown>
+                    </Card.Body>
+                  </Col>
+                </Row>
+              </Container>
+            </Card>
           </Col>
         </Row>
       </Container>
