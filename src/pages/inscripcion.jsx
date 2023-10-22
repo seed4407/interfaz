65a5478a526087abd936263fe091cbfe37debbf0
Benja <benjamin.rivero@usm.com>
2023-10-22 17:45:47 -0300

    hugo

--- src/pages/inscripcion.jsx
+++ src/pages/inscripcion.jsx
@@ -5,11 +5,34 @@ import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
-// import RUT from 'rut.js';
+import RUT from 'rut.js';
+import Modal from 'react-bootstrap/Modal';
 
 export const Inscripcion = () => {
+  const [nombre, setNombre] = useState('');
+  const [apellido1, setApellido1] = useState('');
+  const [apellido2, setApellido2] = useState('');
+  const [birthdate, setBirthdate] = useState('');
+  const [region, setRegion] = useState('');
+  const [comuna, setComuna] = useState('');
+  const [direccion, setDireccion] = useState('');
+  const [email, setEmail] = useState('');
+  const [telefono, setTelefono] = useState('');
+  const [nombreColegio, setNombreColegio] = useState('');
+  const [regionColegio, setRegionColegio] = useState('');
+  const [comunaColegio, setComunaColegio] = useState('');
+  const [egreso, setEgreso] = useState('');
+  const [prom1, setProm1] = useState('');
+  const [prom2, setProm2] = useState('');
+  const [prom3, setProm3] = useState('');
+  const [prom4, setProm4] = useState('');
+  const [file, setFile] = useState(0);
+  const [carrera, setCarrera] = useState('');
   const [rut, setRut] = useState('');
   const [rutError, setRutError] = useState('');
+  const [botonClicado, setBotonClicado] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+
   const handleRutChange = (event) => {
     const inputValue = event.target.value;
     setRut(inputValue);
@@ -26,6 +49,19 @@ export const Inscripcion = () => {
     }
   };
 
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    // Verificar si todos los campos están completos
+    if (nombre && apellido1 && apellido2 && birthdate && region && comuna && direccion && email && telefono && nombreColegio && 
+      regionColegio && comunaColegio && egreso && prom1 && prom2 && prom3 && prom4 && file && rut) {
+        setShowModal(true);
+    } else {
+      // Mostrar un mensaje de error en color rojo
+      setBotonClicado(true);
+    }
+  };
+
   return (
     <div className='page'>
       <NavBar />
@@ -37,17 +73,17 @@ export const Inscripcion = () => {
         <Row className="mb-3">
           <Form.Group as={Col} controlId="Nombre">
             <Form.Label>Nombre</Form.Label>
-            <Form.Control/>
+            <Form.Control value={nombre} onInput={e => setNombre(e.target.value)} />
           </Form.Group>
 
           <Form.Group as={Col} controlId="PrimerApellido">
             <Form.Label>Primer apellido</Form.Label>
-            <Form.Control/>
+            <Form.Control value={apellido1} onInput={e => setApellido1(e.target.value)} />
           </Form.Group>
 
           <Form.Group as={Col} controlId="SegundoApellido">
             <Form.Label>Segundo apellido</Form.Label>
-            <Form.Control/>
+            <Form.Control value={apellido2} onInput={e => setApellido2(e.target.value)} />
           </Form.Group>
         </Row>
 
@@ -68,54 +104,54 @@ export const Inscripcion = () => {
 
           <Form.Group as={Col} controlId="Fecha de nacimiento">
             <Form.Label>Fecha de nacimiento</Form.Label>
-            <Form.Control type="date" />
+            <Form.Control type="date" value={birthdate} onInput={e => setBirthdate(e.target.value)} />
           </Form.Group>
         </Row>
 
         <Row className="mb-3" style={{ paddingTop: '25px' }}>
           <Form.Group as={Col} controlId="Region">
             <Form.Label>Región</Form.Label>
-            <Form.Select defaultValue="Elegir">
-              <option></option>
-              <option>Región de Arica y Parinacota</option>
-              <option>Región de Tarapacá</option>
-              <option>Región de Antofagasta</option>
-              <option>Región de Atacama</option>
-              <option>Región de Coquimbo</option>
-              <option>Región de Valparaíso</option>
-              <option>Región Metropolitana de Santiago</option>
-              <option>Región del Libertador General Bernardo O'Higgins</option>
-              <option>Región del Maule</option>
-              <option>Región del Ñuble</option>
-              <option>Región del Biobío</option>
-              <option>Región de La Araucanía</option>
-              <option>Región de Los Ríos</option>
-              <option>Región de Los Lagos</option>
-              <option>Región de Aysén del General Carlos Ibáñez del Campo</option>
-              <option>Región de Magallanes y de la Antártica Chilena</option>
+            <Form.Select value={region} onChange={(e) => setRegion(e.target.value)}>
+              <option defaultValue=""></option>
+              <option value="Arica">Región de Arica y Parinacota</option>
+              <option value="Tarapaca">Región de Tarapacá</option>
+              <option value="Antofagasta">Región de Antofagasta</option>
+              <option value="Atacama">Región de Atacama</option>
+              <option value="Coquimbo">Región de Coquimbo</option>
+              <option value="Valparaiso">Región de Valparaíso</option>
+              <option value="Santiago">Región Metropolitana de Santiago</option>
+              <option value="OHiggins">Región del Libertador General Bernardo O'Higgins</option>
+              <option value="Maule">Región del Maule</option>
+              <option value="Nuble">Región del Ñuble</option>
+              <option value="Biobio">Región del Biobío</option>
+              <option value="Araucania">Región de La Araucanía</option>
+              <option value="Rios">Región de Los Ríos</option>
+              <option value="Lagos">Región de Los Lagos</option>
+              <option value="Aysen">Región de Aysén del General Carlos Ibáñez del Campo</option>
+              <option value="Magallanes">Región de Magallanes y de la Antártica Chilena</option>
             </Form.Select>
           </Form.Group>
 
           <Form.Group as={Col} controlId="Comuna">
             <Form.Label>Comuna</Form.Label>
-            <Form.Control />
+            <Form.Control value={comuna} onInput={e => setComuna(e.target.value)} />
           </Form.Group>
         </Row>
 
         <Form.Group className="mb-3" controlId="Direccion">
           <Form.Label>Dirección</Form.Label>
-          <Form.Control/>
+          <Form.Control value={direccion} onInput={e => setDireccion(e.target.value)} />
         </Form.Group>
 
         <Row className="mb-3" style={{ paddingTop: '25px' }}>
           <Form.Group as={Col} xs={7} controlId="CorreoElectronico">
             <Form.Label>Correo electrónico</Form.Label>
-            <Form.Control placeholder="example@email.com" type="email"/>
+            <Form.Control placeholder="example@email.com" type="email" value={email} onInput={e => setEmail(e.target.value)} />
           </Form.Group>
 
           <Form.Group as={Col} controlId="Telefono">
             <Form.Label>Teléfono</Form.Label>
-            <Form.Control type="tel"/>
+            <Form.Control type="tel" value={telefono} onInput={e => setTelefono(e.target.value)} />
           </Form.Group>
         </Row>
 
@@ -128,58 +164,58 @@ export const Inscripcion = () => {
           <Row className="mb-3">
             <Form.Group as={Col} controlId="ColegioRegión">
             <Form.Label>Región de tu colegio</Form.Label>
-            <Form.Select defaultValue="Elegir">
-              <option></option>
-              <option>Región de Arica y Parinacota</option>
-              <option>Región de Tarapacá</option>
-              <option>Región de Antofagasta</option>
-              <option>Región de Atacama</option>
-              <option>Región de Coquimbo</option>
-              <option>Región de Valparaíso</option>
-              <option>Región Metropolitana de Santiago</option>
-              <option>Región del Libertador General Bernardo O'Higgins</option>
-              <option>Región del Maule</option>
-              <option>Región del Ñuble</option>
-              <option>Región del Biobío</option>
-              <option>Región de La Araucanía</option>
-              <option>Región de Los Ríos</option>
-              <option>Región de Los Lagos</option>
-              <option>Región de Aysén del General Carlos Ibáñez del Campo</option>
-              <option>Región de Magallanes y de la Antártica Chilena</option>
+            <Form.Select value={regionColegio} onChange={(e) => setRegionColegio(e.target.value)}>
+              <option defaultValue=""></option>
+              <option value="Arica">Región de Arica y Parinacota</option>
+              <option value="Tarapaca">Región de Tarapacá</option>
+              <option value="Antofagasta">Región de Antofagasta</option>
+              <option value="Atacama">Región de Atacama</option>
+              <option value="Coquimbo">Región de Coquimbo</option>
+              <option value="Valparaiso">Región de Valparaíso</option>
+              <option value="Santiago">Región Metropolitana de Santiago</option>
+              <option value="OHiggins">Región del Libertador General Bernardo O'Higgins</option>
+              <option value="Maule">Región del Maule</option>
+              <option value="Nuble">Región del Ñuble</option>
+              <option value="Biobio">Región del Biobío</option>
+              <option value="Araucania">Región de La Araucanía</option>
+              <option value="Rios">Región de Los Ríos</option>
+              <option value="Lagos">Región de Los Lagos</option>
+              <option value="Aysen">Región de Aysén del General Carlos Ibáñez del Campo</option>
+              <option value="Magallanes">Región de Magallanes y de la Antártica Chilena</option>
             </Form.Select>
             </Form.Group>
 
             <Form.Group as={Col} controlId="ColegioComuna">
               <Form.Label>Comuna de tu colegio</Form.Label>
-              <Form.Control />
+              <Form.Control value={comunaColegio} onInput={e => setComunaColegio(e.target.value)} />
             </Form.Group>
           </Row>
 
           <Row className="mb-3">
             <Form.Group as={Col} xs={9} controlId="ColegioNombre">
               <Form.Label>Nombre de tu colegio</Form.Label>
-              <Form.Control />
+              <Form.Control value={nombreColegio} onInput={e => setNombreColegio(e.target.value)} />
             </Form.Group>
 
             <Form.Group as={Col} controlId="EgresoYear">
               <Form.Label>Año de egreso</Form.Label>
-              <Form.Control />
+              <Form.Control value={egreso} onInput={e => setEgreso(e.target.value)} />
             </Form.Group>
           </Row>
 
           <h6>Promedios (Ejemplo: 6,3)</h6>
           <Row className="mb-3">
             <Form.Group as={Col} controlId="Prom1">
-              <Form.Control style={{ fontSize: '13px' }} placeholder="Primero Medio" />
+              <Form.Control style={{ fontSize: '13px' }} placeholder="Primero Medio" value={prom1} onInput={e => setProm1(e.target.value)} />
             </Form.Group>
             <Form.Group as={Col} controlId="Prom2">
-              <Form.Control style={{ fontSize: '13px' }} placeholder="Segundo Medio" />
+              <Form.Control style={{ fontSize: '13px' }} placeholder="Segundo Medio" value={prom2} onInput={e => setProm2(e.target.value)} />
             </Form.Group>
             <Form.Group as={Col} controlId="Prom3">
-              <Form.Control style={{ fontSize: '13px' }} placeholder="Tercero Medio" />
+              <Form.Control style={{ fontSize: '13px' }} placeholder="Tercero Medio" value={prom3} onInput={e => setProm3(e.target.value)} />
             </Form.Group>
             <Form.Group as={Col} controlId="Prom4">
-              <Form.Control style={{ fontSize: '13px' }} placeholder="Cuarto Medio" />
+              <Form.Control style={{ fontSize: '13px' }} placeholder="Cuarto Medio" value={prom4} onInput={e => setProm4(e.target.value)} />
             </Form.Group>
           </Row>
 
@@ -188,18 +224,18 @@ export const Inscripcion = () => {
         <Form style={{ border: '0.5px solid #0463a2', padding: '15px', borderRadius: '10px', backgroundColor: '#fff', marginTop: '5px'}}>
         <h6>Adjunta una imagen de tu cédula de identidad por ambos lados en formato PDF, la cédula debe estar vigente.</h6>
           <Form.Group controlId="pdf">
-            <Form.Control type="file" accept="application/pdf"/>
+            <Form.Control type="file" accept="application/pdf" onChange={(e) => setFile(e.target.files.length)} />
           </Form.Group>
         </Form>
 
         <Form style={{ border: '0.5px solid #0463a2', padding: '15px', paddingBottom: '27px', borderRadius: '10px', backgroundColor: '#fff', marginTop: '5px'}}>
           <Form.Group controlId="EgresoYear">
             <Form.Label>Escoge la carrera a la que quieres postular</Form.Label>
-              <Form.Select defaultValue="Elegir">
-                <option></option>
-                <option>Ingeniería Civil Informática</option>
-                <option>Ingeniería Civil Matemáticas</option>
-                <option>Ingeniería Civil en Minas</option>
+              <Form.Select value={carrera} onChange={(e) => setCarrera(e.target.value)}>
+                <option defaultValue=""></option>
+                <option value="Informatica">Ingeniería Civil Informática</option>
+                <option value="Matematicas">Ingeniería Civil Matemáticas</option>
+                <option value="Minas">Ingeniería Civil en Minas</option>
               </Form.Select>
             </Form.Group>
           </Form>
@@ -207,10 +243,31 @@ export const Inscripcion = () => {
       </Col>
       </Row>
     </Container>
+    {botonClicado && (
+        <center>
+          <p style={{ color: 'red', marginBottom: '-20px', marginTop: '20px' }}>
+            ¡Completa todos los campos antes de enviar!
+          </p>
+        </center>
+      )}
     <center>
-      <Button style={{ marginTop: '25px'}} variant="primary" type="submit" size="lg">
+      <Button style={{ marginTop: '25px'}} variant="primary" type="submit" size="lg" onClick={handleFormSubmit}>
           Postular
         </Button>
+        
+      <Modal show={showModal} onHide={() => setShowModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Formulario enviado exitosamente</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          ¡Tu formulario ha sido enviado exitosamente!
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShowModal(false)}>
+            Cerrar
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </center>
 
     </div>
